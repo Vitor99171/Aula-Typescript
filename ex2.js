313f31156f9c475b1138657d3da9dfc2e4d6a00e
Vitor99171 <vitormanzolli99@gmail.com>
2024-08-08 09:48:08 -0300

    hete

---
+++ ex2.js
@@ -0,0 +1,6 @@
+var usuario = {
+    nome: 'hete caetano',
+    idade: 200,
+    email: 'hetemetepatinete@hete.hete'
+};
+console.log("hetesuário:", usuario);
---
+++ ex2.ts
@@ -0,0 +1,13 @@
+interface Usuario {
+    nome : string;
+    idade : number;
+    email : string;
+}
+
+let usuario : Usuario = {
+    nome : 'hete caetano',
+    idade : 200,
+    email : 'hetemetepatinete@hete.hete'
+};
+
+console.log("hetesuário:", usuario)
