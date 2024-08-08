24421ee7a30e60f43a00a8df62ecb6582d71a1a4
Vitor99171 <vitormanzolli99@gmail.com>
2024-08-08 09:38:38 -0300

    emilly loca

---
+++ ex1.js
@@ -0,0 +1,8 @@
+// Exercício 1: Declaração de Variáveis
+var idade = 39;
+var nome = 'hete caetano';
+var isAtivo = true;
+// Imprimir 
+console.log("Idade:", idade);
+console.log("Nome:", nome);
+console.log("IsAtivo:", isAtivo);
---
+++ ex1.ts
@@ -0,0 +1,10 @@
+// Exercício 1: Declaração de Variáveis
+
+let idade: number = 39;
+let nome: string = 'hete caetano';
+let isAtivo: boolean = true;
+
+// Imprimir 
+console.log("Idade:", idade);
+console.log("Nome:", nome);
+console.log("IsAtivo:", isAtivo);
